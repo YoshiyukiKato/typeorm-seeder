b8f998bc95e8c1a4ca25b5601acfcb2ca2cad1be
YoshiyukiKato <yo.shuuuuki@gmail.com>
2019-05-23 16:41:22 +0900

    support one-to-* relations

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import {
   ConnectionOptions,
   Connection,
   Repository,
+  getMetadataArgsStorage,
 } from "typeorm";
 import glob from "fast-glob";
+import { RelationMetadataArgs } from "typeorm/metadata-args/RelationMetadataArgs";
 
 export interface ISeed {
   Entity: { new (): any };
   seeds: any[];
 }
 
+type TEntityRelations = { [key: string]: RelationMetadataArgs[] };
+
 export class Seeder {
   private connection: Connection;
+  private basePath: string;
   public async init(
     this: Seeder,
     configName?: string,
@@ -23,51 +28,104 @@ export class Seeder {
       basePath || ".",
       configName || "ormconfig.json"
     );
+    this.basePath = basePath;
     this.connection = await createConnection(<ConnectionOptions>(
       require(configPath)
     ));
     return this;
   }
 
-  public async applySeed(
+  public async applySeeds(
     this: Seeder,
     { Entity, seeds }: ISeed,
     clear: boolean = false
-  ): Promise<void> {
+  ): Promise<any[]> {
+    const entityRelations = getMetadataArgsStorage()
+      .relations.filter(({ target, relationType }) => {
+        return target === Entity && relationType.match(/-to-many$/);
+      })
+      .reduce((acc: TEntityRelations, relation: RelationMetadataArgs) => {
+        acc[relation["propertyName"]] = acc[relation["propertyName"]] || [];
+        acc[relation["propertyName"]].push(relation);
+        return acc;
+      }, {});
+
+    const relAppliedSeeds = await this.applyRelactions(
+      seeds,
+      entityRelations,
+      clear
+    );
     const repository = this.connection.getRepository(Entity);
+
+    return await (clear
+      ? this.removeSeeds(repository, relAppliedSeeds)
+      : this.saveSeeds(repository, relAppliedSeeds));
+  }
+
+  public async applyRelactions(
+    seeds: any[],
+    entityRelations: TEntityRelations,
+    clear: boolean
+  ) {
+    const relAppliedSeeds = [];
     for (const seed of seeds) {
-      await (clear
-        ? this.removeSeed(repository, seed)
-        : this.saveSeed(repository, seed));
+      const relAppliedSeed = Object.assign({}, seed);
+      for (const [key, val] of Object.entries(seed)) {
+        if (!entityRelations[key]) continue;
+        for (const entityRelation of entityRelations[key]) {
+          const ChildEntity = (<Function>entityRelation.type)();
+          if (Array.isArray(val)) {
+            relAppliedSeed[key] = await this.applySeeds(
+              <ISeed>{
+                Entity: ChildEntity,
+                seeds: val,
+              },
+              clear
+            );
+          } else {
+            relAppliedSeed[key] = (await this.applySeeds(
+              <ISeed>{
+                Entity: ChildEntity,
+                seeds: [val],
+              },
+              clear
+            ))[0];
+          }
+        }
+      }
+      relAppliedSeeds.push(relAppliedSeed);
     }
+    return relAppliedSeeds;
   }
 
-  async saveSeed<Entity>(
+  async saveSeeds<Entity>(
     this: Seeder,
     repository: Repository<Entity>,
-    seed: any
+    seeds: any[]
   ) {
-    const entity = repository.create(seed);
-    await repository.save(entity);
-    console.info("Saved seed: ", seed);
+    const entities = repository.create(seeds);
+    await repository.save(entities);
+    console.info("Saved seeds: ", seeds);
+    return entities;
   }
 
-  async removeSeed<Entity>(
+  async removeSeeds<Entity>(
     this: Seeder,
     repository: Repository<Entity>,
-    seed: any
+    seeds: any[]
   ) {
-    const entity = repository.create(seed);
-    await repository.remove(entity);
-    console.info("Removed seed: ", seed);
+    const entities = repository.create(seeds);
+    await repository.remove(entities);
+    console.info("Removed seed: ", seeds);
+    return entities;
   }
 
   public async loadSeed(this: Seeder, seedData: ISeed): Promise<void> {
-    await this.applySeed(seedData);
+    await this.applySeeds(seedData);
   }
 
   public async clearSeed(this: Seeder, seedData: ISeed): Promise<void> {
-    await this.applySeed(seedData, true);
+    await this.applySeeds(seedData, true);
   }
 
   public async loadSeedByFiles(
@@ -89,7 +147,10 @@ export class Seeder {
     filePatterns: string[],
     clear: boolean = false
   ): Promise<void> {
-    const seedDataList = await this.getSeedDataByFiles(filePatterns);
+    const absFilePatterns = filePatterns.map(filePattern =>
+      path.resolve(this.basePath, filePattern)
+    );
+    const seedDataList = await this.getSeedDataByFiles(absFilePatterns);
     for (const seedData of seedDataList) {
       if (clear) {
         await this.clearSeed(seedData);
